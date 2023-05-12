0430a95ee7ef2a739f8e9f236a28f814850358c5
Gabriel Kleiman <kleim4n@gmail.com>
2023-05-11 23:57:56 -0300

    feat/criado projeto spring-boot

---
+++ src/test/java/com/kleiman/votingspring/VotingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleiman.votingspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
